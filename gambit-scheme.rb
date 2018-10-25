31ae7ec61d14f16bd650948b33f5d18db3f5bc5e
ober <ober@linbsd.org>
2018-10-25 16:25:53 -0700

    fix installs

--- gambit-scheme.rb
+++ gambit-scheme.rb
@@ -24,7 +24,7 @@ class GambitScheme < Formula
     system "make"
     ENV.deparallelize
     #system "make", "install"
-    bins = %W[
+    %w[
          gambcomp-C
          gambcomp-java
          gambcomp-js
@@ -34,51 +34,40 @@ class GambitScheme < Formula
          gambdoc
          gsc
          gsi
-         ]
-    gsc_links = %W[ gsc-script ]
-    gsi_links = %W[
-                  gsi-script
-                  scheme-ieee-1178-1990
-                  scheme-r4rs
-                  scheme-r5rs
-                  scheme-srfi-0
-                  six
-                  six-script ]
+         ].each do |b|
+            bin.install Dir["bin/#{b}"]
+         end
 
-    docs = %W[ gambit.html gambit.pdf gambit.txt ]
-    includes = %W[ gambit-not409000.h  gambit.h ]
-    infos = %W[ gambit.info gambit.info-1 gambit.info-2 gambit.info-3 ]
-    libs = %W[ _asm#.scm _define-syntax.scm  _gambit.c _io#.scm _num#.scm _syntax-boot.scm _syntax-common.scm _syntax-template.scm _syntax.scm _with-syntax-boot.scm  digest.scm libgambitgsc.a  r5rs#.scm
-    _assert#.scm  _eval#.scm _gambitgsc.c  _kernel#.scm  _repl#.scm  _syntax-case-xform-boot.scm  _syntax-pattern.scm _syntax-xform-boot.scm  _system#.scm  _x86#.scm gambit#.scm  libgambitgsi.a  syntax-case.scm
-    _codegen#.scm  _gambit#.scm _gambitgsi.c  _nonstd#.scm  _std#.scm _syntax-case-xform.scm _syntax-rules-xform.scm  _syntax-xform.scm _thread#.scm  digest#.scm libgambit.a  r4rs#.scm ]
-
-    bins.each do |b|
-      bin.install Dir["bin/#{b}"]
-    end
-
-    gsc_links.each do |l|
+    %w[ gsc-script ].each do |b|
       bin.install_symlink libexec"bin/gsc" => "#{l}"
     end
 
-    gsi_links.each do |l|
-      bin.install_symlink libexec"bin/gsi" => "#{l}"
-    end
+    %w[
+        gsi-script
+        scheme-ieee-1178-1990
+        scheme-r4rs
+        scheme-r5rs
+        scheme-srfi-0
+        six
+        six-script ].each do |l|
+            bin.install_symlink libexec"bin/gsi" => "#{l}"
+        end
 
-    docs.each do |d|
+    %W[ gambit.html gambit.pdf gambit.txt ].each do |d|
       doc.install Dir["doc/#{d}"]
     end
 
-    includes.each do |i|
+    %W[ gambit-not409000.h  gambit.h ].each do |i|
       include.install Dir["include/#{i}"]
     end
-
-    infos.each do |i|
+    %W[ gambit.info gambit.info-1 gambit.info-2 gambit.info-3 ].each do |i|
       info.install Dir["info/#{i}"]
     end
-
-    libs.each do |l|
+    %W[ _asm#.scm _define-syntax.scm  _gambit.c _io#.scm _num#.scm _syntax-boot.scm _syntax-common.scm _syntax-template.scm _syntax.scm _with-syntax-boot.scm  digest.scm libgambitgsc.a  r5rs#.scm
+    _assert#.scm  _eval#.scm _gambitgsc.c  _kernel#.scm  _repl#.scm  _syntax-case-xform-boot.scm  _syntax-pattern.scm _syntax-xform-boot.scm  _system#.scm  _x86#.scm gambit#.scm  libgambitgsi.a  syntax-case.scm
+    _codegen#.scm  _gambit#.scm _gambitgsi.c  _nonstd#.scm  _std#.scm _syntax-case-xform.scm _syntax-rules-xform.scm  _syntax-xform.scm _thread#.scm  digest#.scm libgambit.a  r4rs#.scm
+    ].each do |l|
       lib.install Dir["lib/#{l}"]
     end
-
   end
 end
