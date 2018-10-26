4d64220feec0519123e738800dba3d8d44ba21ab
ober <ober@linbsd.org>
2018-10-25 22:25:21 -0700

    fix path

--- datadog.rb
+++ datadog.rb
@@ -11,9 +11,7 @@ class Datadog < Formula
     ENV.append_path "PATH", "#{Formula['gerbil-scheme'].bin}"
     ENV.prepend "LDFLAGS", "-L/usr/local/lib/"
     ENV.prepend "LDFLAGS", "-L#{Formula['openssl'].lib}/openssl"
-    puts "XXX lib is -L#{Formula['openssl'].lib}"
-    ENV.prepend "CPPFLAGS", "-I/usr/local/include/openssl"
-    puts "XXX include is -I#{Formula['openssl'].include}"
+    ENV.prepend "CPPFLAGS", "-I/usr/local/include"
     ENV.prepend "CPPFLAGS", "-I#{Formula['openssl'].include}"
     system "./build.ss static"
     bin.install Dir["./datadog"]
