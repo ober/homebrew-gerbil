4085f160ace1225516e3949b2a58a28e264a38e1
ober <jaimef@linbsd.org>
2018-11-18 15:08:05 -0800

    use head

--- gerbil-scheme.rb
+++ gerbil-scheme.rb
@@ -1,9 +1,9 @@
 class GerbilScheme < Formula
   desc "An opinionated dialect of Scheme designed for Systems Programming"
   homepage "https://cons.io"
-  url "https://github.com/vyzo/gerbil/archive/v0.13.tar.gz"
-  sha256 "b464579f82682d733752a40a3e164fb387de94e05f220d04bfb43f2a70db40e3"
-  head "https://github.com/vyzo/gerbil.git"
+  url "https://github.com/vyzo/gerbil.git"
+#  sha256 "b464579f82682d733752a40a3e164fb387de94e05f220d04bfb43f2a70db40e3"
+#  head "https://github.com/vyzo/gerbil.git"
 
   depends_on "gambit-scheme"
   depends_on "gcc@6"
